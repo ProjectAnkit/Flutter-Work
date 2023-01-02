47166ea9c4501d3298bcc55c1ec670e562d812a3
Ankit Thakur <xankitxthakurx@gmail.com>
2023-01-03 01:03:18 +0530

    Update LoginPageTrial1.dart

--- UI dev. Phase/LoginPageTrial1.dart
+++ UI dev. Phase/LoginPageTrial1.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,53 +13,103 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/LoginImage1.jpeg"),fit: BoxFit.fill)),
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: const BoxDecoration(image: DecorationImage(image: AssetImage("assets/LoginImage1.jpeg"),fit: BoxFit.fill)),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
              height: 180,
             ),
             Center(child: Text("FIRE",style:GoogleFonts.langar(color: Colors.red,fontSize: 50)
             )
             ),
-            Text("Welcome User",style: GoogleFonts.jost(fontSize: 23,color: Color.fromARGB(255, 247, 129, 19),fontWeight: FontWeight.w500),),
-            SizedBox(
+            Text("Welcome User",style: GoogleFonts.jost(fontSize: 23,color: const Color.fromARGB(255, 247, 129, 19),fontWeight: FontWeight.w500),),
+            const SizedBox(
               height: 20,
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(45)),
-              child: TextField(
+              child: const TextField(
                 cursorColor: Colors.black,
                 cursorHeight:20,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(45)),
-              child: TextField(
+              child: const TextField(
                 cursorColor: Colors.black,
                 cursorHeight: 20,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
-            Container(
-              height: 40,
-              width: 100,
-              decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.circular(45)),
-              child: Center(child: Text("Sign in")),
-            )
+            const NewButton(),
           ],
         ),
       ),
     );
+  }
+}
+
+class NewButton extends StatefulWidget {
+  const NewButton({super.key});
+
+  @override
+  State<NewButton> createState() => _NewButtonState();
+}
+
+class _NewButtonState extends State<NewButton> {
+
+  bool Buttonpressed = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+              onTap: () async {
+                setState((){
+                  Buttonpressed = false;
+                },
+                );
+                await Future.delayed(const Duration(seconds: 3));
+                
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: 40,
+                width: 100,
+                decoration: BoxDecoration(color: Colors.red,
+                  borderRadius: BorderRadius.circular(45),
+                    boxShadow:Buttonpressed?[const BoxShadow(
+                      offset: Offset(4,4),
+                      blurRadius: 2,
+                      spreadRadius: 1,
+                      color: Colors.black,
+                    ),
+                     const BoxShadow(
+                      offset: Offset(-2,-2),
+                      blurRadius: 2,
+                      spreadRadius: 1,
+                      color: Colors.white,
+                    ),]:[],
+                    ),
+                child: Center(
+                  child: Buttonpressed?Text("Sign in",
+                  style: GoogleFonts.lato(
+                    color: Colors.white),
+                ):Container(
+                  height:16,
+                  width: 16,
+                  child:CircularProgressIndicator(strokeWidth: 3,color: Colors.white,)),
+                ),
+              ),
+            );
   }
 }
