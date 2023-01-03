54e77ad923217fe61294861bb261b36e05c5ba2a
Ankit Thakur <xankitxthakurx@gmail.com>
2023-01-03 22:27:37 +0530

    Update LoginPageTrial1.dart

--- UI dev. Phase/LoginPageTrial1.dart
+++ UI dev. Phase/LoginPageTrial1.dart
@@ -1,3 +1,4 @@
+import 'package:classico1/Routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,46 +14,86 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(image: DecorationImage(image: AssetImage("assets/LoginImage1.jpeg"),fit: BoxFit.fill)),
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(
-             height: 180,
-            ),
-            Center(child: Text("FIRE",style:GoogleFonts.langar(color: Colors.red,fontSize: 50)
-            )
-            ),
-            Text("Welcome User",style: GoogleFonts.jost(fontSize: 23,color: const Color.fromARGB(255, 247, 129, 19),fontWeight: FontWeight.w500),),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(45)),
-              child: const TextField(
-                cursorColor: Colors.black,
-                cursorHeight:20,
-                decoration: InputDecoration(border: InputBorder.none),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+             
+              const SizedBox(
+                 height: 200,
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(45)),
-              child: const TextField(
-                cursorColor: Colors.black,
-                cursorHeight: 20,
-                decoration: InputDecoration(border: InputBorder.none),
+              const Icon(Icons.lock,size: 65,color: Colors.red,),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const NewButton(),
-          ],
+              // ignore: prefer_const_constructors
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal:8.0),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Username or Mobile number",
+                        border: InputBorder.none,
+                        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        fillColor: Colors.black12,
+                        filled: true)
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal:8.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: InputBorder.none,
+                        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        fillColor: Colors.black12,
+                        filled: true)
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.topLeft,
+                child: Text("Forgot Password ?",style: GoogleFonts.lato(color: Colors.red),textAlign: TextAlign.left,)),
+              const SizedBox(
+                height: 14,
+              ),
+              const NewButton(),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text("  or continue with  ",style: GoogleFonts.lato(),),
+                  const Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Profiles(),
+            ],
+          ),
         ),
       ),
     );
@@ -79,37 +120,70 @@ class _NewButtonState extends State<NewButton> {
                 },
                 );
                 await Future.delayed(const Duration(seconds: 3));
+                Navigator.pushNamed(context, MyRoutes.Homepage);
                 
               },
               child: AnimatedContainer(
                 duration: const Duration(milliseconds: 300),
-                height: 40,
-                width: 100,
+                height: 60,
+                width: 340,
                 decoration: BoxDecoration(color: Colors.red,
-                  borderRadius: BorderRadius.circular(45),
+                  borderRadius: BorderRadius.circular(5),
                     boxShadow:Buttonpressed?[const BoxShadow(
-                      offset: Offset(4,4),
+                      offset: Offset(2,2),
                       blurRadius: 2,
                       spreadRadius: 1,
                       color: Colors.black,
                     ),
-                     const BoxShadow(
-                      offset: Offset(-2,-2),
-                      blurRadius: 2,
-                      spreadRadius: 1,
-                      color: Colors.white,
-                    ),]:[],
+                    ]:[],
                     ),
                 child: Center(
                   child: Buttonpressed?Text("Sign in",
                   style: GoogleFonts.lato(
+                    fontSize: 25,
                     color: Colors.white),
                 ):Container(
                   height:16,
                   width: 16,
-                  child:CircularProgressIndicator(strokeWidth: 3,color: Colors.white,)),
+                  child:const CircularProgressIndicator(strokeWidth: 2,color: Colors.white,)),
                 ),
               ),
             );
+  }
+}
+
+class Profiles extends StatelessWidget {
+  const Profiles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15)),
+            child: Image(image: AssetImage("assets/Google.png",))),
+            SizedBox(
+              width: 20,
+            ),
+            Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15)),
+            child: Image(image: AssetImage("assets/Apple.png",))),
+            SizedBox(
+              width: 20,
+            ),
+            Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15)),
+            child: Image(image: AssetImage("assets/Gmail.png",))),
+        ],
+      ),
+    );
   }
 }
